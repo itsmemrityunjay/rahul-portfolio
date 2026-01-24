0a8c7e918ded713e6c2e6f53a2479b4e8ead3d3c
Abhishek <141233909+itsmemrityunjay@users.noreply.github.com>
2026-01-24 21:03:51 +0530

    lenis

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,18 @@ import TechStack from "./components/TechStack";
 import About from "./components/About";
 import Showcase from "./components/Showcase";
 import Work from "./components/Work";
+import Lenis from 'lenis'
+
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
 
 export default function Home() {
   return (
