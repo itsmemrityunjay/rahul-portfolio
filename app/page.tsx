6d73d19d6849297fc9fee9959b014313bd7f3890
Abhishek <141233909+itsmemrityunjay@users.noreply.github.com>
2026-01-24 21:05:11 +0530

    zxcvbn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect } from "react";
 import Hero from "./components/Hero";
 import TechStack from "./components/TechStack";
 import About from "./components/About";
@@ -5,18 +8,24 @@ import Showcase from "./components/Showcase";
 import Work from "./components/Work";
 import Lenis from 'lenis'
 
-
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
-
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
-
 export default function Home() {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+
+    // Listen for the scroll event and log the event data
+    lenis.on('scroll', (e) => {
+      console.log(e);
+    });
+
+    // Cleanup function
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="bg-black  min-h-screen">
       <Hero />
